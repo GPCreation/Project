5be1b470bb9f4972ea43ac817f9846de7166fb5c
Gaurav <pandeygaurav0805@gmail.com>
2023-05-18 11:37:54 +0530

    intial commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,142 @@
+import React from 'react'
+import "../_next/static/css/7f6c37248e409200.css"
+import "../_next/static/css/39fcafa6a58c2c1d.css"
+import "../_next/static/css/878f47798519344a.css"
+import "../_next/static/css/3563a7cb7daddf04.css"
+import "../_next/static/css/889109f2bc9196f6.css"
+import "../_next/static/css/18037183ce7df910.css"
+
+export default function Navbar() {
+    return (
+        <div>
+           
+    <div id="__next">
+        <div class="loading_wrapper__PSQrr loading fixed top-0 left-0 z-[99] w-full">
+            <div  class="flex w-full items-center justify-center relative z-[2]">
+                <div class="max-w-[210px]">
+                    <div>
+                        <div>
+                            <div></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+      
+        <div class="header_wrapper__8VLPZ">
+            <div class="container container--full">
+                <div class="header_container__cv77f">
+                    <div class="header_logo__x2KMD" data-state="closed" >
+                        <div class="header_logo__inner__NWekb"><button class="header_logo__x2KMD"
+                                data-scroll-to="#top"><span class="pointer-events-none">
+                                    <div>
+                                        <div>
+                                            <div></div>
+                                        </div>
+                                    </div>
+                                </span></button></div>
+                    </div>
+                  
+                   
+               
+                </div>
+            </div>
+        </div>
+        <div id="top"></div>
+        <div class="page-wrapper">
+            <div class="page-content">
+                <section class="hero_section__jloWD">
+                    <div class="hero_container__azrgD">
+                        <div class="hero_inner__eeq8d">
+                            <div class="hero_wrapper__yWXfS container container--expanded">
+                                <div class="relative z-[1]">
+                               
+                                    
+                                    <div class="absolute w-[82%] top-[18%] left-[7%] flex">
+                                       
+                                      
+                                    </div>
+                                    <div class="absolute z-[2] top-0 right-0 h-[5.6%] flex items-center">
+                                        <div>
+                                            <div>
+                                                <div></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div
+                                        class="hero_screens__LDm6X absolute z-[2] top-0 right-[.8%] w-[4.7%] h-full flex flex-col items-center justify-center gap-2">
+                                        <div><img src="#" alt="" class="" /></div>
+                                        <div><img src="#" alt="" class="" /></div>
+                                        <div>
+                                            <div class=""><button class="relative">
+                                                    <div
+                                                        class="absolute w-full h-full flex items-center justify-center">
+                                                        <img src="#" alt="" class="w-[20%] h-auto" /></div><img src="#"
+                                                        alt="" class="" />
+                                                </button></div>
+                                        </div>
+                                    </div><img src="#" alt="" class="w-full h-auto" />
+                                </div>
+                            </div>
+                        </div>
+                        <div class="hero_bg__QQMHb"><img src="#" alt="" class="" /></div>
+                        <div class="hero_placeholder__qbINg"></div>
+                    </div>
+                  
+                </section>
+              
+                <div class="ui_separator__LFIMb">
+                    <div class="container"><span></span></div>
+                </div>
+               
+                <section class="textreveal_section__Dw9vK  ">
+                    <div class="container">
+                        <div class="textreveal_wrapper__fuEcC">
+                            <div class="textreveal_sticky__NBmkY">
+                                <div class="textreveal_inner__Kddvp">
+                                    <div class="relative">
+                                        <p class="text-white title-lead font-semibold leading-none absolute">In
+                                            Chronicle everything is made with Blocks that come with pixel perfect
+                                            design, interactivity and motion out of the box. Instead of designing from
+                                            scratch, simply choose the right one from our library of blocks and see the
+                                            magic unfold.</p>
+                                        <p class="title-lead font-semibold leading-none text-[#202020]">In Chronicle
+                                            everything is made with Blocks that come with pixel perfect design,
+                                            interactivity and motion out of the box. Instead of designing from scratch,
+                                            simply choose the right one from our library of blocks and see the magic
+                                            unfold.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="textreveal_text__9qSpD"></div>
+                        </div>
+                    </div>
+                </section>
+              
+                <div class="ui_separator__LFIMb">
+                    <div class="container"><span></span></div>
+                </div>
+              
+                <div class="ui_separator__LFIMb">
+                    <div class="container"><span></span></div>
+                </div>
+               
+              
+            
+              
+            </div>
+            <div class="pagebottomspacer_wrapper__zrLBD"></div>
+        </div><span class="cursor_cursor__ERu1j cursor" data-cursor="true"><svg width="16" height="18"
+                viewBox="0 0 16 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.0695 9.03414L0.663119 1.11451L3.74331 17.2631L7.34918 10.3171L15.0695 9.03414Z"
+                    fill="#000000" stroke="#000000" stroke-linejoin="round"></path>
+            </svg><span>You</span></span>
+       
+    </div>
+    
+   </div>
+
+
+        
+    )
+}
